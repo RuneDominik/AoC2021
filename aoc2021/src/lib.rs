4183f2e9902a4c2f54793e4cce051a91d05af1e8
Rune Michael Dominik <rune.dominik@udo.edu>
2021-12-03 10:15:59 +0100

    Changed absolute to relative paths

--- aoc2021/src/lib.rs
+++ aoc2021/src/lib.rs
@@ -8,7 +8,7 @@ mod sonar_tests {
 
         let pings = vec![199i64, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        let read_pings = sonar::get_pings("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day1_data/test_data.txt").unwrap();
+        let read_pings = sonar::get_pings("day1_data/test_data.txt").unwrap();
 
         assert_eq!(pings, read_pings);
     }
@@ -17,7 +17,7 @@ mod sonar_tests {
     fn test_count_increasing() {
         use crate::submarine::sonar;
 
-        let counts = sonar::count_increasing("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day1_data/test_data.txt").unwrap();
+        let counts = sonar::count_increasing("day1_data/test_data.txt").unwrap();
 
         assert_eq!(counts, 7);
     }
@@ -26,7 +26,7 @@ mod sonar_tests {
     fn count_increasing_windowsum() {
         use crate::submarine::sonar;
 
-        let counts = sonar::count_increasing_windowsum("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day1_data/test_data.txt").unwrap();
+        let counts = sonar::count_increasing_windowsum("day1_data/test_data.txt").unwrap();
 
         assert_eq!(counts, 5);
     }
@@ -38,7 +38,7 @@ mod piloting_tests {
     fn test_get_course_auto() {
         use crate::submarine::piloting;
 
-        let course = piloting::get_course_auto("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day2_data/test_data.txt").unwrap();
+        let course = piloting::get_course_auto("day2_data/test_data.txt").unwrap();
 
         assert_eq!(course, 150);
     }
@@ -46,7 +46,7 @@ mod piloting_tests {
     fn test_get_course_manual() {
         use crate::submarine::piloting;
 
-        let course = piloting::get_course_manual("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day2_data/test_data.txt").unwrap();
+        let course = piloting::get_course_manual("day2_data/test_data.txt").unwrap();
 
         assert_eq!(course, 900);
     }
--- aoc2021/src/main.rs
+++ aoc2021/src/main.rs
@@ -5,17 +5,17 @@ use crate::submarine::piloting;
 
 fn main() {
     println!("=================== Day 1 ===================");
-    let count_simple = sonar::count_increasing("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day1_data/data.txt").unwrap();
+    let count_simple = sonar::count_increasing("day1_data/data.txt").unwrap();
     println!("The simple count is increating {} times", count_simple);
 
-    let count_window = sonar::count_increasing_windowsum("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day1_data/data.txt").unwrap();
+    let count_window = sonar::count_increasing_windowsum("day1_data/data.txt").unwrap();
     println!("The window count is increating {} times", count_window);
 
     println!("=================== Day 2 ===================");
-    let course_auto = piloting::get_course_auto("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day2_data/data.txt").unwrap();
+    let course_auto = piloting::get_course_auto("day2_data/data.txt").unwrap();
     println!("The auto course is {}", course_auto);
 
-    let course_maual = piloting::get_course_manual("/home/runedominik/Desktop/Sonst/AoC2021/aoc2021/day2_data/data.txt").unwrap();
+    let course_maual = piloting::get_course_manual("day2_data/data.txt").unwrap();
     println!("The manual course is {}", course_maual);
 
     println!("=================== Day 3 ===================");
